ecb97bc9885924ba587491c7f2d025eedd83b29a
ajcorte <ajcorte@gmail.com>
2023-12-27 19:25:02 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 let playerName = "Agnaldo";
 
-let nivelHeroi = "";
+let nivelHero = "";
 
 let pontosXP = "8368";
 
 if (pontosXP <= 1000){
-  nivelHeroi =  "Ferro";
+  nivelHero =  "Ferro";
 }
 if (pontosXP > 1000 && pontosXP < 2001){
-  nivelHeroi = "Bronze";
+  nivelHero = "Bronze";
 }
 if (pontosXP > 2000 && pontosXP < 5001) {
-  nivelHeroi = "Prata";
+  nivelHero = "Prata";
 }
 if (pontosXP > 5000 && pontosXP < 7001){
-  nivelHeroi = "Ouro";
+  nivelHero = "Ouro";
 }
 if (pontosXP > 7000 && pontosXP < 8001){
-  nivelHeroi = "Platina";
+  nivelHero = "Platina";
 }
 if (pontosXP > 8000 && pontosXP < 9001){
-  nivelHeroi = "Ascendente";
+  nivelHero = "Ascendente";
 }
 if (pontosXP > 9000 && pontosXP < 10001){
-  nivelHeroi = "Imortal";
+  nivelHero = "Imortal";
 }
 if (pontosXP >= 10001){
-  nivelHeroi = "Radiante";
+  nivelHero = "Radiante";
 }
-console.log("O jogador " + playerName + " tem " + pontosXP + " pontos de XP e esta no nivel " + nivelHeroi +"!");
+console.log("O jogador " + playerName + " tem " + pontosXP + " pontos de XP e esta no nivel " + nivelHero +"!");
 
